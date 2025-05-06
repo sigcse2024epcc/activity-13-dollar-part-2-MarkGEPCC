b50bf720fe473ebcef09c8e7e01b7964d9dca710
MarkGEPCC <mgon1091@my.epcc.edu>
2025-05-06 14:19:19 -0600

    Update MoneyProcessor.java

--- MoneyProcessor.java
+++ MoneyProcessor.java
@@ -1,5 +1,48 @@
-public class MoneyProcessor{
+import java.utiil.Scanner;
+import java.io.*;
+
+public class MoneyProcessor throws IOException{
   public static void main(String[] args){
+    Scanner input = new Scanner(new File("money.txt");
+    while(input.hasNext)){
+      String line = input.nextLine();
+      String [] tokens = line.split(",");
+        Dollar bill = new Dollar(Integer.parseInt(token[0]), tokens[1], tokens[2], Integer.parseInt(tokens[3]));
+        bill.showBill();
+    }
+
+    input = new Scanner(new File("money.txt));
+    Dollar [] wallet = new Dollar[10];
+    for(int i = 0; input.hasNext(); i++){
+      String line = input.nextLine();
+      String [] tokens = line.split(",");
+      Dollar bill = new Dollar(Integer.parseInt(token[0]), tokens[1], tokens[2], Integer.parseInt(tokens[3]));
+      wallet[i] = bill;
+    }
+    System.out.print("**** printBills() ****");
+    printBills(wallet);
+    System.out.print("**** printOnlyDenominations() ****");
+    printOnlyDenominations(wallet);
+    System.out.print("**** getTotalAmountInWallet ****");
+    System.out.print(getTotalAmountInWallet(wallet));
     
+  }
+  public static void printBills(Dollar [] array){
+    for(Dollar d: array){
+      d.showBill();
+    }
+  }
+  public static void printOnlyDenominations(Dollar[] array){
+    for(int i = 0; i < array.length; i++){
+    System.out.print(array[i].getDenomination() + " ")
+      }
+    System.out.println();
+  }
+  public static int getTotalAmountInWallet(Dollar [] array){
+    int sum = 0;
+    for(int i = 0; i < array.length; i++){
+      sum += array[i].getDenomination();
+    }
+    return sum;
   }
 }
